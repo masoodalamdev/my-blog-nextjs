ad8c312f8ab157b0356b5046c46de920ce63782c
masoodalamdev <107204700+masoodalamdev@users.noreply.github.com>
2024-11-13 20:17:29 +0500

    some changes

--- app/(components)/Navbar.jsx
+++ app/(components)/Navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar() {
     <nav className="navbar">
         <div className="logo">My Blog</div>
         <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+        <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
         </ul>
         <div className="menu-toggle" onClick={()=> {document.querySelector(".nav-links").classList.toggle("active")}}>&#9776;</div>
     </nav>
